98567e20fd51d4759720c84079c31acd7f9e7d8a
zendaokaofficial <110347285+zendaokaofficial@users.noreply.github.com>
2023-09-18 06:16:16 +0800

    Update index.py
    Add summary per subsector

--- index.py
+++ index.py
@@ -4,6 +4,42 @@ from datetime import date
 import time
 from streamlit_extras.metric_cards import style_metric_cards
 import numpy as np
+from math import pi
+import altair as alt
+
+def show(sheet_db, Filter):
+
+    st.subheader("Summary")
+    a = []
+    b = []
+    c = []
+
+    url_1 = sheet_db.replace('/edit#gid=', '/export?format=csv&gid=')
+    df = pd.read_csv(url_1, on_bad_lines='skip')
+    len_db = len(df[df["Kategori"]==f"{Filter}"]["Kategori"])
+
+    ##Looping 
+    for z in range(len_db):
+        sheet_url2 = df.iloc[z,2]
+        url_2 = sheet_url2.replace('/edit#gid=', '/export?format=csv&gid=')
+        x = pd.read_csv(url_2, on_bad_lines='skip') #
+        #x.iloc[:,:-3] = x.iloc[:,:-3].applymap(str)
+
+        len_x = len(x['Sudah Diperbaiki'])
+        ksk_true = x['Sudah Diperbaiki'].sum()
+        opr_true = x['Sudah di entry'].sum()
+        a.append(len_x)
+        b.append(ksk_true)
+        c.append(opr_true) 
+    
+    a_sum = sum(a)
+    b_sum = sum(b)
+    c_sum = sum(c)
+
+    col1, col2 = st.columns(2)
+    col1.metric(label = "Persentase Sudah Diperbaiki", value = f"{round(b_sum/a_sum * 100, 2)} %", delta = f"{b_sum} ruta")
+    col2.metric(label = "Persentase Sudah Dientry", value = f"{round(c_sum/a_sum * 100, 2)} %", delta = f"{c_sum} ruta")
+    style_metric_cards(border_left_color = '#1E1E1E')
 
 ## Read
 sheet_url = "https://docs.google.com/spreadsheets/d/1fwOqEmIXqRq43hjELFF61DpxNkYIdk7vK_mzoM-NfWM/edit#gid=0"
@@ -31,7 +67,13 @@ with st.sidebar:
 
         SecondFilter = st.selectbox("Anomali", lstAnomali, 0)
 
-if FirstFilter != "Pilih Kategori" and SecondFilter != "Pilih Anomali":
+if FirstFilter == "Pilih Kategori":
+    st.title("Selamat Datang di SiAnom")
+    st.text("SiAnom memberikan informasi mengenai Anomali ST2023 di BPS Kabupaten Tabanan. Untuk melihat Summary tiap-tiap subsektor silahkan pilih Subsektor pada sidebar di sebelah kiri")
+elif FirstFilter != "Pilih Kategori" and SecondFilter == "Pilih Anomali":
+    show(sheet_url, FirstFilter)
+
+elif FirstFilter != "Pilih Kategori" and SecondFilter != "Pilih Anomali":
 
     sheet_url2 = df2[df2["Anomali"] == SecondFilter].iloc[0,2]
     url_2 = sheet_url2.replace('/edit#gid=', '/export?format=csv&gid=')
@@ -60,14 +102,17 @@ if FirstFilter != "Pilih Kategori" and SecondFilter != "Pilih Anomali":
     n1 = len(df3['Sudah Diperbaiki'])
     n2 = len(df3['Sudah di entry'])
 
-    m1 = (round(p1/n1 * 100, 2))
-    m2 = (round(p2/n2 * 100, 2))
-        
-    col1.metric(label = "Persentase Sudah Diperbaiki", value = f"{str(m1)} %")
-    col2.metric(label = "Persentase Sudah Dientry", value = f"{str(m2)} %")
+    col1.metric(label = "Persentase Sudah Diperbaiki", value = f"{round(p1/n1 * 100, 2)} %")
+    col2.metric(label = "Persentase Sudah Dientry", value = f"{round(p2/n2 * 100, 2)} %")
     style_metric_cards(border_left_color = '#1E1E1E')
     
     if len(columns) > 0:
 
         st.checkbox("Use container width", value=False, key="use_container_width")
         st.dataframe(df4, use_container_width=st.session_state.use_container_width)
+        
+
+## Untuk Tampilan Tabel
+# df = pd.read_csv(url_1, on_bad_lines='skip')
+# df.iloc[:,:-3] = df.iloc[:,:-3].applymap(str)
+# df
